05e66aa55cbd1fa214822f3a5045ac83dc7ee4ba
Vivian Li <li.v.cheerful@gmail.com>
2016-05-23 21:30:16 -0500

    linking

--- temp/script.js
+++ temp/script.js
@@ -1,6 +1,6 @@
 
 var main = function(){
-	$('<a href="index.html">').text("Vivian").prependTo('.navbar');
+	$('<a href="../index.html">').text("Vivian").prependTo('.navbar');
 	$('<a href="gems.html">').text("Gems Computer Science Camp for Girls").appendTo('.navbar');
 	$('<a href="comics.html">').text("Comics").appendTo('.navbar');
 	$('<a href="blog.html">').text("Blog").appendTo('.navbar');
